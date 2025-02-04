cb14644a81829ec30cd0af974801d8b74dda077d
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2025-02-04 17:02:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+
 function fun(){
-            var a=document.getElementById("hide");
-            if(a.style.right=="20px"){
-                a.style.right="-60%";
-                a.style.transition="2s";
-            }else{
-            a.style.right="20px";
-            a.style.transition="2s";
-            }
-            
-        }
+    var a=document.getElementById("hide");
+    if(a.style.right=="20px"){
+        a.style.right="-60%";
+        a.style.transition="2s";
+    }else{
+    a.style.right="20px";
+    a.style.transition="2s";
+    }
+    
+}
 function hid(){
-            var a=document.getElementById("hide");
-            a.style.right="-60%";
-            a.style.transition="2s";
-        }
+    var a=document.getElementById("hide");
+    a.style.right="-60%";
+    a.style.transition="2s";
+}
