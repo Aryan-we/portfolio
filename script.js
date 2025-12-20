f2fd011031a4cec6432fd802f02c54ed885ea7e6
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2025-12-20 15:23:04 +0530

    Reduce transition duration in fun and hid functions
    Updated transition duration from 2s to 1s in functions.

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ function fun(){
     var a=document.getElementById("hide");
     if(a.style.right=="20px"){
         a.style.right="-60%";
-        a.style.transition="2s";
+        a.style.transition="1s";
     }else{
     a.style.right="20px";
-    a.style.transition="2s";
+    a.style.transition="1s";
     }
     
 }
 function hid(){
     var a=document.getElementById("hide");
     a.style.right="-60%";
-    a.style.transition="2s";
+    a.style.transition="1s";
 }
+
 
 
 
