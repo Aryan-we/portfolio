0ebd75f0dd9afb747ac2c783ba4fcb9f11a3497f
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2025-12-20 14:54:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function fun(){
         a.style.transition="2s";
     }else{
     a.style.right="20px";
-    a.style.transition="2s";
+    a.style.transition="1s";
     }
     
 }
 function hid(){
     var a=document.getElementById("hide");
     a.style.right="-60%";
-    a.style.transition="2s";
+    a.style.transition="1s";
 }
+
